c4c81ada82cccc6c066e594a38ee9d9dca4aa0c5
Drew Stinnett <drew.stinnett@duke.edu>
2019-12-27 18:07:16 -0500

    Adding in Vim skeleton for python

---
+++ .vim/templates/skel.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+
+
+def parse_args():
+    """Parse the arguments."""
+    parser = argparse.ArgumentParser(
+        description="This is my super sweet script")
+    parser.add_argument("-v",
+                        "--verbose",
+                        help="Be verbose",
+                        action="store_true",
+                        dest="verbose")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(args)
+    @CURSOR@
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
