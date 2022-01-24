9c34c0d6dad52036d03bed8baa174a13828a29e5
Drew Stinnett <drew@drewlink.com>
2022-01-24 14:27:44 -0500

    Adding op config

--- .config/fish/conf.d/op.fish
+++ .config/fish/conf.d/op.fish
@@ -6,3 +6,15 @@ function op_logout
     set -e OP_SESSION_dukeu
     set -e OP_EXPIRATION_TIME
 end
+
+function op_login_home
+    set tok (op signin my --raw)
+    if [ $status -eq 0 ]
+      echo "Tight, you are logged in 😁"
+      set -Ux OP_SESSION_my $tok
+      set -U OP_EXPIRATION_TIME (date -v +30M -u +%s)
+    else
+      echo "1password login failed 😢" 1>&2
+      return 2
+    end
+end
