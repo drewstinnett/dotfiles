8f8e58efc1c7f526b51cb83b9f476ffa04027270
Drew Stinnett <drew@drewlink.com>
2023-09-12 20:20:24 -0400

    More keyboard shortcuts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,13 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = 'AdventureTime'
--- config.color_scheme = 'Solarized (light) (terminal.sexy)'
 config.color_scheme = 'Solarized Light (Gogh)'
 config.font_size = 14.0
 config.font = wezterm.font 'Source Code Pro for Powerline'
--- config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.hide_mouse_cursor_when_typing = false
@@ -26,44 +15,17 @@ config.default_cursor_style = 'BlinkingBar'
 config.selection_word_boundary = " \t\n={[}]()\"'"
 
 config.window_frame = {
-  -- The font used in the tab bar.
-  -- Roboto Bold is the default; this font is bundled
-  -- with wezterm.
-  -- Whatever font is selected here, it will have the
-  -- main font setting appended to it to pick up any
-  -- fallback fonts you may have used there.
   font = wezterm.font { family = 'Roboto', weight = 'Bold' },
-
-  -- The size of the font in the tab bar.
-  -- Default to 10.0 on Windows but 12.0 on other systems
   font_size = 12.0,
-
-  -- The overall background color of the tab bar when
-  -- the window is focused
-  -- active_titlebar_bg = '#ffffd7',
   active_titlebar_bg = '#FDF6E3',
-
-  -- The overall background color of the tab bar when
-  -- the window is not focused
   inactive_titlebar_bg = '#D6D1BC',
 }
 
--- config.use_fancy_tab_bar = false
 config.colors = {
   tab_bar = {
-    -- The color of the strip that goes along the top of the window
-    -- (does not apply when fancy tab bar is in use)
-    -- background = '#FDF6E3',
-    -- background = '#D3CBB8',
-    -- background = '#D9D2C3',
-
-    -- The active tab is the one that has focus in the window
     active_tab = {
       bg_color = '#FDF6E3',
       fg_color = '#555451',
-      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-      -- label shown for this tab.
-      -- The default is "Normal"
       intensity = 'Bold',
       underline = 'None',
       italic = false,
@@ -107,31 +69,37 @@ config.colors = {
 }
 
 config.keys = {
-    {key="P", mods="CTRL",
-     action=wezterm.action{QuickSelectArgs={
-       patterns={
+    {
+      key = "P", mods="CTRL",
+      action=wezterm.action{QuickSelectArgs={
+      patterns={
           "https?://\\S+"
-       },
-       action = wezterm.action_callback(function(window, pane)
+      },
+      action = wezterm.action_callback(function(window, pane)
           local url = window:get_selection_text_for_pane(pane)
           wezterm.log_info("opening: " .. url)
           wezterm.open_with(url)
-       end)
+      end)
      }}
    },
    {
-    key = 't',
-    mods = 'CMD',
-    action = wezterm.action.SpawnCommandInNewTab {
-       cwd = wezterm.home_dir
+      key = 't', mods = 'CMD',
+      action = wezterm.action.SpawnCommandInNewTab {
+        cwd = wezterm.home_dir
+      },
     },
-  },
+    {
+      key = 'f', mods = 'SHIFT|CMD',
+      action = wezterm.action.ToggleFullScreen,
+    },
+    {
+      key = 'p', mods = 'SHIFT|CMD',
+      action = wezterm.action.ActivateCommandPalette,
+    },
+
+
 
 }
 
-
-
-
 -- and finally, return the configuration to wezterm
 return config
-
